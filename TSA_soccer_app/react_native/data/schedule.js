ed3849aa24c9a9b3a15258f9ce0b1d3f5629cc19
Johan Sebastiampillai <johan15_seb@hotmail.com>
2021-06-05 00:56:48 -0400

    Add data models

---
+++ TSA_soccer_app/react_native/data/schedule.js
@@ -0,0 +1,16 @@
+import Activity from '../models/activity';
+
+export const Schedule = [
+    new Activity(0, 'Practice', 'confirmed', 'Ramerville Park', 6479724551, new Date(2020, 3, 4, 17, 40, 0)),
+    new Activity(1, 'Practice', 'pending', 'Ramerville Park', 6479724551, new Date(2020, 3, 5, 17, 40, 0)),
+    new Activity(2, 'Practice', 'pending', 'Ramerville Park', 6479724551, new Date(2020, 3, 6, 17, 40, 0)),
+    new Activity(3, 'Practice', 'pending', 'Ramerville Park', 6479724551, new Date(2020, 3, 7, 17, 40, 0)),
+    new Activity(4, 'Practice', 'confirmed', 'Ramerville Park', 6479724551, new Date(2020, 4, 8, 17, 40, 0)),
+    new Activity(5, 'Practice', 'confirmed', 'Ramerville Park', 6479724551, new Date(2020, 4, 24, 17, 40, 0)),
+    new Activity(6, 'Practice', 'confirmed', 'Ramerville Park', 6479724551, new Date(2020, 4, 24, 17, 40, 0)),
+    new Activity(7, 'Practice', 'confirmed', 'Ramerville Park', 6479724551, new Date(2020, 4, 24, 17, 40, 0)),
+    new Activity(8, 'Practice', 'confirmed', 'Ramerville Park', 6479724551, new Date(2020, 5, 24, 17, 40, 0)),
+    new Activity(9, 'Practice', 'cancelled', 'Ramerville Park', 6479724551, new Date(2020, 5, 24, 17, 40, 0)),
+    new Activity(10, 'Practice', 'cancelled', 'Ramerville Park', 6479724551, new Date(2020, 5, 24, 17, 40, 0)),
+    new Activity(11, 'Practice', 'cancelled', 'Ramerville Park', 6479724551, new Date(2020, 5, 24, 17, 40, 0)),
+]
