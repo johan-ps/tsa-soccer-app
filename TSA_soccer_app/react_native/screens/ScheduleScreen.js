f4740ce20e3f208bf99acb22d19d7e9b7a3a16d6
joell02 <joelljps@hotmail.com>
2021-06-07 10:35:51 -0400

    Schedule screen updated

--- TSA_soccer_app/react_native/screens/ScheduleScreen.js
+++ TSA_soccer_app/react_native/screens/ScheduleScreen.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import AddButton from '../components/AddButton';
+import LabeledTextInput from '../components/LabeledTextInput';
 
 const ScheduleScreen = () => {
+
+  const [input, setInput] = useState("");
+  const [input2, setInput2] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Schedule</Text>
+      <LabeledTextInput
+        value={input}
+        placeholder="Enter text ..."
+        onChangeText={text => setInput(text)}
+        borderTheme={'underline'}>
+      </LabeledTextInput>
+      <LabeledTextInput
+        value={input2}
+        placeholder="Enter text ..."
+        onChangeText={text => setInput2(text)}
+        borderTheme={'circle'}>
+      </LabeledTextInput>
+
       <AddButton />
     </View>
   );
@@ -18,7 +36,7 @@ const styles = StyleSheet.create({
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'black',
+    backgroundColor: 'white',
   },
   text: {
     color: 'red'
