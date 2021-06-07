9281449572064c3084dff89f852d1696e59d91ce
Johan Sebastiampillai <johan15_seb@hotmail.com>
2021-06-06 20:17:57 -0400

    Android fixes

--- TSA_soccer_app/react_native/components/PlayerListItem.js
+++ TSA_soccer_app/react_native/components/PlayerListItem.js
@@ -1,60 +1,64 @@
 import { ThemeProvider } from '@react-navigation/native';
 import React, { useState } from 'react';
-import { Text, TextInput, StyleSheet, View, Image, Animated, Easing, Pressable, TouchableHighlight } from 'react-native';
+import {
+  Text,
+  TextInput,
+  StyleSheet,
+  View,
+  Image,
+  Animated,
+  Easing,
+  Pressable,
+  TouchableHighlight,
+} from 'react-native';
 import { useSelector } from 'react-redux';
 
 const PlayerListItem = props => {
-
-  const { player, navigation } = props
+  const { player, navigation } = props;
   const theme = useSelector(state => state.theme.colors);
 
   return (
-    <View>
-      <TouchableHighlight
-        onPress={() => navigation.navigate('PlayerProfile')}
-        style={{width: '100%'}}
-      >
-        <View
-          style={[styles.container, player.last ? {borderBottomWidth: 0} : null]}
-        >
-          <Image 
-            style={styles.logo}
-            source={{ uri: player.image}}
-          />
-          <Text
-            style={styles.text}
-          >{player.name}</Text>
-        </View>
-      </TouchableHighlight>
-    </View>
+    <TouchableHighlight
+      onPress={() => navigation.navigate('PlayerProfile')}
+      style={styles.touchableContainer}>
+      <View
+        style={[styles.container, player.last ? { borderBottomWidth: 0 } : {}]}>
+        <Image style={styles.logo} source={{ uri: player.image }} />
+        <Text style={styles.text}>{player.name}</Text>
+      </View>
+    </TouchableHighlight>
   );
-  
 };
 
 const styles = StyleSheet.create({
-    container: {
-      width: '100%',
-      height: 60,
-      display: 'flex',
-      flexDirection: 'row',
-      position: 'relative',
-      alignItems: 'center',
-      backgroundColor: 'white', 
-      borderBottomColor: '#C0C0C0',
-      borderBottomWidth: 0.5
-    },
-    logo: {
-      height: 40,
-      width: 40,
-      borderRadius: 25,
-      marginTop: 20,
-      marginBottom: 20,
-      marginRight: 20
-    },
-    text: {
-      fontSize: 16,
-      fontWeight: '500'
-    }
+  touchableContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    position: 'relative',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderBottomColor: '#C0C0C0',
+    borderBottomWidth: 0.2,
+    borderStyle: 'solid',
+    padding: 10,
+    paddingHorizontal: 15,
+  },
+  logo: {
+    height: 40,
+    width: 40,
+    borderRadius: 25,
+    marginRight: 20,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
 });
 
 export default PlayerListItem;
